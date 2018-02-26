ea8ea1614445f2589d1da626974315182627a36d
Sergei Boreisha <sboreisha@exadel.com>
2018-02-26 09:48:51 +0300

    first commit

---
+++ src/test/java/com/exadel/automation/test.java
@@ -0,0 +1,15 @@
+package com.exadel.automation;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+/**
+ * Created by sboreisha on 2/26/2018.
+ */
+public class test extends TestBase {
+
+    @Test
+    public void testTest(){
+        Assert.assertTrue(false);
+    }
+}
